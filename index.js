8b9371bc41a96f8ea5e778ff17f9a7e96cd48a67
Youssef Albokl <88749793+Boklino@users.noreply.github.com>
2023-01-27 10:48:14 +0200

    m

--- index.js
+++ index.js
@@ -2,28 +2,61 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const Person = require("./models/person");
+const { Model } = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(403).send({ error: "unknown endpoint" });
+};
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((person) => {
     response.json(person);
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) response.json(person);
+      else response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(203).end();
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -44,11 +77,17 @@ app.post("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${length} people </p> <p>  ${Date()} </p>`
-  );
+  Person.find({}).then((person) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        person.length
+      } people </p> <p>  ${Date()} </p>`
+    );
+  });
 });
 
+app.use(unknownEndpoint);
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
